0616012745d6d1c2861eff4ef221d0b0242130a1
Reza <rezafafaa78@gmail.com>
2023-11-16 13:47:08 +0330

    footer and navbar has been maded

--- src/Containers/Layout/Footer.jsx
+++ src/Containers/Layout/Footer.jsx
@@ -1,7 +1,86 @@
+import { Box, Button, Grid, Typography, createTheme, styled } from "@mui/material";
 import React from "react";
+import TelegramIcon from '@mui/icons-material/Telegram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
-const Footer = () => {
-  return <div>Footer</div>;
+
+
+
+const FooterButton =styled("button")(({theme}) =>({
+  all:"unset",
+  color:"#E3E8E7ff",
+  fontSize:"18px",
+  backgroundColor:"none",
+  border:"none",
+  fontWeight:"100",
+
+  
+  '&:hover':{
+    borderBottom:"1px solid white",
+   
+    cursor:"pointer"
+  }
+}))
+
+const Footer = (props) => {
+
+
+
+
+  return(
+    
+    <>
+    <Grid sx={{backgroundColor:"#072541"}} container>
+      {/* footer buttons */}
+<Grid mt={4} container display = "flex" justifyContent="space-around" alignItems="center">
+
+<Grid xs={5} sm={4} md={4} alignItems="center" display="flex" flexDirection="column" sx={{marginLeft:{xs:"8%",sm:"0"}}}>
+<Box  mb={2}><FooterButton variant="standard">صفحه اصلی</FooterButton></Box>
+<Box mb={2}><FooterButton variant="standard"> درباره ما</FooterButton></Box>
+<Box ><FooterButton variant="standard">  شرایط و قوانین</FooterButton></Box>
+
+</Grid>
+
+<Grid whiteSpace="nowrap" xs={5} sm={4} md={4} alignItems="center" display="flex" flexDirection="column">
+<Box mb={2}><FooterButton variant="standard"> سوالات متداول</FooterButton></Box>
+<Box mb={2}><FooterButton variant="standard">  چطوری به طلایار اعتماد کنیم</FooterButton></Box>
+<Box ><FooterButton variant="standard">   ارتباط با ما </FooterButton></Box>
+
+</Grid>
+
+
+<Grid xs={2} sm={4} md={2} sx={{mt:{xs:"10%",sm:"0"}}}>
+  <img width={100} height={100} src={props.image}/>
+</Grid>
+
+</Grid>
+
+
+<Grid mt={4} mb={4} container display="flex"  justifyContent="space-around" alignItems="center">
+  <Grid item display="flex" alignItems="center" whiteSpace="nowrap" sx={{ml:{xs:"2%",sm:"0"}}}>
+    <Typography color="white" fontSize={20} variant="h5">آدرس :</Typography><Typography color="#fff" variant="h6" mr={1} fontWeight="bold">زنجان سفر اباد خیابان 12 نبش نانوایی</Typography>
+  </Grid>
+
+
+  <Grid item display="flex" flexDirection="column" alignItems="center">
+    <Box display="flex" alignItems="center"> 
+      <Typography color="white" fontSize={20} >  تلفن : </Typography>
+      <Typography mr={1} color="#fff" variant="h6" >024-2424-2424-24</Typography>
+    </Box>
+
+    <Box mt={1} display="flex" alignItems="center" >
+<TelegramIcon sx={{ml:3 ,color:"#fff",cursor:"pointer",border:"1px solid white",padding:"5px",transition:'all ease 1s' ,'&:hover':{borderRadius:"50%"}}}/>
+<WhatsAppIcon sx={{ml:3,color:"#fff",cursor:"pointer",border:"1px solid white",padding:"5px",transition:'all ease 1s' ,'&:hover':{borderRadius:"50%"}}}/>
+<InstagramIcon sx={{ml:3,color:"#fff",cursor:"pointer",border:"1px solid white",padding:"5px",transition:'all ease 1s' ,'&:hover':{borderRadius:"50%"}}}/>
+    </Box>
+  </Grid>
+</Grid>
+
+
+    </Grid>
+    </>
+  );
 };
 
 export default Footer;
--- src/Containers/Layout/NavBar.jsx
+++ src/Containers/Layout/NavBar.jsx
@@ -1,7 +1,137 @@
+import { AppBar, Avatar, Box, Button, Container, Grid, IconButton, Menu, Toolbar, Tooltip, Typography } from "@mui/material";
 import React from "react";
+import AdbIcon from '@mui/icons-material/Adb';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
-const NavBar = () => {
-  return <div>NavBar</div>;
+const pages = ['ارتباط با ما', 'سوالات متداول', 'درباره ما'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+
+const NavBar = (props) => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  return(
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{display:{sm:"none" ,md:"block"}}}>
+         <img  width={40} height={40} src={props.image}/>
+         </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              ml:4,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            طلایار
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}>
+         <img  width={40} height={40} src={props.image}/>
+         </Box>
+          
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            طلایار
+          </Typography>
+          <Box sx={{ flexGrow: 2, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2,mx:2, color: 'white', display: 'block' ,fontSize:"20px" ,'&:hover':{borderBottom:"1px solid white"} }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="ثبت نام">
+              <Button sx={{color:"#FFC436",border:"1px solid #FFC436" ,'&:hover':{backgroundColor:"rgba(204, 163, 69,0.6)"}}}>ورود | ثبت نام</Button>
+            </Tooltip>
+           
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default NavBar;
