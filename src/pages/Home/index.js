e0ae985d1c9f19a48ed76a0df12955a6a8ba2f05
emad <arabanemad@gmail.com>
2023-11-24 20:23:43 +0330

    Update PackageJson & Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,6 @@
-import HomePhoneNumber from "@/Components/AddPhoneNumber";
+import { Animate } from "@/Components/Animate/Animate";
+import ArrowBottom from "@/Components/ArrowBottom/ArrowBottom";
+import DimondHeader from "@/Components/DimondHeader/DimondHeader";
 import StepperComputerList from "@/Components/StepperComputer";
 import StepperList from "@/Components/StepperPhone";
 import TabPriceList from "@/Components/TabPrice";
@@ -11,16 +13,31 @@ const HomeLand = () => {
     <>
       <Layout>
         <Container maxWidth="xl">
-          <Grid container justifyContent="space-around">
+          <Grid ml={10} my={24} xs={12}>
+            <DimondHeader />
+          </Grid>
+          <Grid
+            ml={5}
+            mt={8}
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%,-50%)",
+            }}
+          >
+            <ArrowBottom />
+          </Grid>
+          <Grid container my={24}>
             <Grid item xs={12} md={6}>
-              <HomePhoneNumber />
+              <Animate />
             </Grid>
             <Grid item xs={12} md={6}>
               <TabPriceList />
             </Grid>
           </Grid>
 
-          <Grid mb={20} mt={20}>
+          <Grid mb={20} mt={50}>
             <StepperComputerList />
             <StepperList />
           </Grid>
