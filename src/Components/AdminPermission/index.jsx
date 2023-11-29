4d2c690b73dd3ba909364050b1804135770b3e99
Reza <rezafafaa78@gmail.com>
2023-11-29 15:52:06 +0330

    Admin permission updated

--- src/Components/AdminPermission/index.jsx
+++ src/Components/AdminPermission/index.jsx
@@ -2,63 +2,38 @@ import { Box, Button, Paper, Typography, Grid } from "@mui/material";
 import Switch from "@mui/material/Switch";
 const AdminPermission = () => {
   return (
-    <Grid
+    <Paper
       sx={{
+        bgcolor: "#272523",
+        maxWidth: "500px",
+        width: "100%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-     
+        borderRadius: "10px",
+        height: "auto",
+        py: 1.5,
+        px: 2,
       }}
     >
-      <Grid
-
+      <Switch
+        defaultChecked
+        sx={{
+          "& .MuiButtonBase-root ": {
+            color: "#FFC436 !important",
+          },
+        }}
+      />
+      <Typography
+        whiteSpace="nowrap"
+        color="green"
+        fontWeight="bold"
+        fontFamily="Yekan"
+        variant="h6"
       >
-        <Paper
-          sx={{
-            bgcolor: "#272523",
-            width: "100%",
-
-            borderRadius: "10px",
-            height: "100%",
-            px: 6,
-          }}
-        >
-          <Grid
-            item
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            direction="ltr"
-            padding="20px 0"
-            flexDirection="column"
-          >
-            <Box mb={4} display="flex">
-              <Switch defaultChecked />
-              <Typography
-                color="green"
-                fontWeight="bold"
-                fontFamily="Yekan"
-                variant="h6"
-              >
-                اجازه ی خرید
-              </Typography>
-            </Box>
-
-            <Box display="flex">
-              <Switch defaultChecked />
-              <Typography
-                color="red"
-                fontWeight="bold"
-                fontFamily="Yekan"
-                variant="h6"
-              >
-                اجازه ی فروش
-              </Typography>
-            </Box>
-          </Grid>
-        </Paper>
-      </Grid>
-    </Grid>
+        اجازه ی خرید و فروش
+      </Typography>
+    </Paper>
   );
 };
 
