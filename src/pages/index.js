891861b9885bf7e14d9f316d52fabe510c9ce232
emad <arabanemad@gmail.com>
2023-12-07 12:22:17 +0330

    Create api SSR in indexMain

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,68 @@
+import HomeLand from "@/Components/Home";
 import Layout from "@/Containers/Layout";
+import axios from "axios";
+import { parseCookies } from "nookies";
+import { IPServer } from "@/Config";
+export default function Home({
+  validationToken,
+  TabPriceTable,
+  validationTokenError,
+  TabPriceTableError,
+}) {
+  return (
+    <Layout dashboard={validationTokenError}>
+      <HomeLand tabPrice={TabPriceTable} />
+    </Layout>
+  );
+}
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const cookies = parseCookies({ req });
+  const token = cookies["auth-token"];
 
-import { Container, Grid } from "@mui/material";
-import TabPriceList from "@/Components/TabPrice";
-import StepperComputerList from "@/Components/StepperComputer";
-import HomePhoneNumber from "@/Components/AddPhoneNumber";
-import StepperList from "@/Components/StepperPhone";
-import HomeLand from "./Home/index";
-export default function Home() {
-  return <HomeLand />;
+  let validationToken;
+  let TabPriceTable;
+  let validationTokenError;
+  let TabPriceTableError;
+
+  if (token) {
+    try {
+      const { data: ValdationToken2 } = await axios.get(
+        `${IPServer}/Home/validate-token/`,
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      );
+
+      validationToken = ValdationToken2;
+      validationTokenError = "200";
+    } catch (error) {
+      validationToken = "";
+      validationTokenError = "400";
+    }
+  } else {
+    validationToken = "";
+    validationTokenError = "400";
+  }
+  try {
+    const { data: TabPriceTable2 } = await axios.get(
+      `${IPServer}/Home/gold-stock-price/`
+    );
+
+    TabPriceTable = TabPriceTable2;
+    TabPriceTableError = "200";
+  } catch (error) {
+    TabPriceTable = "";
+    TabPriceTableError = "400";
+  }
+  return {
+    props: {
+      validationToken,
+      TabPriceTable,
+      validationTokenError,
+      TabPriceTableError,
+    },
+  };
 }
