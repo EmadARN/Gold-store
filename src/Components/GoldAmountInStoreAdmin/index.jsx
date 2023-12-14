ed82036b79caa4264dfb3a13661f3145387de47f
emad <arabanemad@gmail.com>
2023-12-14 15:12:30 +0330

    Add api GoldAmountStoreAdmin

--- src/Components/GoldAmountInStoreAdmin/index.jsx
+++ src/Components/GoldAmountInStoreAdmin/index.jsx
@@ -1,14 +1,73 @@
 import GoldAmount from "./GoldAmount";
-import { Grid, Box, Paper } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 import AdminPermission from "../AdminPermission";
-const GoldAmountMain = () => {
+import { useState } from "react";
+import axios from "axios";
+import { IPServer } from "@/Config";
+import { useCookies } from "react-cookie";
+
+const GoldAmountMain = ({ settingData }) => {
+  const [goldPrice, setGoldPrice] = useState();
+  const [cookies] = useCookies(["token"]);
+  const [priceDifference, setPriceDifference] = useState();
+
+  // changeGoldPrice
+  const changeGoldPrice = (event) => {
+    const newValue = { ...goldPrice, value: event.target.value };
+
+    setGoldPrice(newValue);
+  };
+  const clickHandlerchangeGoldPrice = async () => {
+    await axios
+      .post(
+        `${IPServer}/AdminDashboard-Setting/change-warehouse-gold-amount/`,
+        {
+          gold_amount: parseFloat(goldPrice.value),
+        },
+        {
+          headers: {
+            Authorization: `Token ${cookies.token}`,
+          },
+        }
+      )
+      .then((res) => {
+        window.location.reload();
+      })
+      .catch((err) => {});
+  };
+
+  // changePriceDifference
+  const changePriceDifference = (event) => {
+    const newValue = { ...priceDifference, value: event.target.value };
+
+    setPriceDifference(newValue);
+  };
+  const clickHandlerchangePriceDifference = async () => {
+    await axios
+      .post(
+        `${IPServer}/AdminDashboard-Setting/change-price-difference/`,
+        {
+          price_difference: parseFloat(priceDifference.value),
+        },
+        {
+          headers: {
+            Authorization: `Token ${cookies.token}`,
+          },
+        }
+      )
+      .then((res) => {
+        window.location.reload();
+      })
+      .catch((err) => {});
+  };
+
   return (
     <>
       <Grid
         container
         display="flex"
         justifyContent="center"
-height="110%"
+        height="110%"
         alignItems="center"
       >
         <Paper
@@ -17,7 +76,7 @@ height="110%"
             justifyContent: "center",
             width: { xs: "100%", md: "90%", lg: "70%" },
 
-          py:8,
+            py: 8,
             mx: { xs: 0, md: 6, lg: 0 },
             alignItems: "center",
             backgroundColor: "rgba(255,255,255,0.1)",
@@ -28,7 +87,7 @@ height="110%"
           }}
         >
           <Grid item mb={5} xs={12}>
-            <AdminPermission />
+            <AdminPermission stock_status={settingData.stock_status} />
           </Grid>
 
           <Grid
@@ -43,6 +102,10 @@ height="110%"
                 inputLabel={"گرم"}
                 buttonText={"تایید"}
                 walletcash={" طلای موجود"}
+                settingData={settingData.total_gold_stock}
+                unit={"گرم"}
+                onChange={(e) => changeGoldPrice(e)}
+                onClick={() => clickHandlerchangeGoldPrice()}
               />
             </Grid>
 
@@ -51,7 +114,11 @@ height="110%"
                 BoxTitle={"اختلاف قیمت خرید و فروش"}
                 inputLabel={"ریال"}
                 buttonText={"تایید"}
-                walletcash={" طلای موجود"}
+                walletcash={"اختلاف قیمت فعلی"}
+                settingData={settingData.price_difference}
+                unit={"ریال"}
+                onChange={(e) => changePriceDifference(e)}
+                onClick={() => clickHandlerchangePriceDifference()}
               />
             </Grid>
           </Grid>
--- src/pages/GoldAmountStoreAdmin/index.js
+++ src/pages/GoldAmountStoreAdmin/index.js
@@ -1,14 +1,57 @@
-import GoldAmountMain from '@/Components/GoldAmountInStoreAdmin'
-import UserDashboard from '@/Containers/DashboardUser/UserDashboard'
-import { DrawerObjAdmin } from '@/Utils/DrawerObjectAdmin'
-import React from 'react'
-
-const GoldAmount = () => {
+import GoldAmountMain from "@/Components/GoldAmountInStoreAdmin";
+import UserDashboard from "@/Containers/DashboardUser/UserDashboard";
+import { DrawerObjAdmin } from "@/Utils/DrawerObjectAdmin";
+import React from "react";
+import axios from "axios";
+import { parseCookies } from "nookies";
+import { IPServer } from "@/Config";
+const GoldAmount = ({ settingData, settingDataError }) => {
   return (
- <UserDashboard DrawerObj={DrawerObjAdmin} indexBtn={6}>
-  <GoldAmountMain/>
- </UserDashboard>
-  )
-}
+    <UserDashboard DrawerObj={DrawerObjAdmin} indexBtn={6}>
+      <GoldAmountMain settingData={settingData.data} />
+    </UserDashboard>
+  );
+};
 
-export default GoldAmount
+export default GoldAmount;
+export async function getServerSideProps(ctx) {
+  const { req } = ctx;
+  const cookies = parseCookies({ req });
+  const token = cookies["token"];
+
+  let settingData;
+  let openClose;
+  let settingDataError;
+  let openCloseError;
+  if (token) {
+    try {
+      const { data: settingData2 } = await axios.get(
+        `${IPServer}/AdminDashboard-Setting/setting-data/`,
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      );
+      settingData = settingData2;
+      settingDataError = "200";
+    } catch (error) {
+      settingData = "";
+      settingDataError = "400";
+    }
+  } else {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/SignUp",
+      },
+    };
+  }
+
+  return {
+    props: {
+      settingData,
+      settingDataError,
+    },
+  };
+}
