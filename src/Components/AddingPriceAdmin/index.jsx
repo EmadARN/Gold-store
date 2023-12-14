4bc2a830943107c9ae4267571daaa33cc468b4ee
emad <arabanemad@gmail.com>
2023-12-14 15:12:02 +0330

    Add api AddingPriceAdmin

--- src/Components/AddingPriceAdmin/index.jsx
+++ src/Components/AddingPriceAdmin/index.jsx
@@ -1,14 +1,16 @@
-import Withdraw from "../Withdraw/Withdraw";
-const AddingPrice = () => {
-    return ( 
-        <Withdraw
-        title={"قیمت گذاری"}
-        BoxTitle={"قیمت را وارد کنید"}
-        inputLabel={"ریال"}
-        buttonText={"تایید"}
-        walletcash ={"  قیمت قبل"}
-        />
-     );
-}
- 
+
+import WithdrawAdmin from "./WithdrawAdmin";
+const AddingPrice = ({ settingData }) => {
+  return (
+    <WithdrawAdmin
+      title={"قیمت گذاری"}
+      BoxTitle={"قیمت را وارد کنید"}
+      inputLabel={"ریال"}
+      buttonText={"تایید"}
+      walletcash={"  قیمت قبل"}
+      walletValue={settingData.sale_price}
+    />
+  );
+};
+
 export default AddingPrice;
--- src/pages/AddingPriceAdmin/index.js
+++ src/pages/AddingPriceAdmin/index.js
@@ -1,14 +1,56 @@
-import AddingPrice from '@/Components/AddingPriceAdmin'
-import UserDashboard from '@/Containers/DashboardUser/UserDashboard'
-import { DrawerObjAdmin } from '@/Utils/DrawerObjectAdmin'
-import React from 'react'
+import AddingPrice from "@/Components/AddingPriceAdmin";
+import { IPServer } from "@/Config";
+import UserDashboard from "@/Containers/DashboardUser/UserDashboard";
+import { DrawerObjAdmin } from "@/Utils/DrawerObjectAdmin";
+import axios from "axios";
+import { parseCookies } from "nookies";
+import React from "react";
 
-const AddingPriceAdmin = () => {
+const AddingPriceAdmin = ({ settingData }) => {
   return (
-  <UserDashboard indexBtn={5} DrawerObj={DrawerObjAdmin}>
-    <AddingPrice/>
-  </UserDashboard>
-  )
-}
+    <UserDashboard indexBtn={5} DrawerObj={DrawerObjAdmin}>
+      <AddingPrice settingData={settingData.data} />
+    </UserDashboard>
+  );
+};
 
-export default AddingPriceAdmin
+export default AddingPriceAdmin;
+export async function getServerSideProps(ctx) {
+  const { req } = ctx;
+  const cookies = parseCookies({ req });
+  const token = cookies["token"];
+
+  let settingData;
+  let settingDataError;
+  if (token) {
+    try {
+      const { data: settingData2 } = await axios.get(
+        `${IPServer}/AdminDashboard-Setting/setting-data/`,
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+        }
+      );
+      settingData = settingData2;
+      settingDataError = "200";
+    } catch (error) {
+      settingData = "";
+      settingDataError = "400";
+    }
+  } else {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/SignUp",
+      },
+    };
+  }
+
+  return {
+    props: {
+      settingData,
+      settingDataError,
+    },
+  };
+}
