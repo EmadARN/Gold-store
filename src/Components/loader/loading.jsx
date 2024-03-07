baa02487306ef356a672aa79a03f0d174ca8dee2
Reza <rezafafaa78@gmail.com>
2024-03-07 14:05:51 +0330

    loading

--- src/Components/loader/loading.jsx
+++ src/Components/loader/loading.jsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 
-export default function CircularIndeterminate() {
+export default function CircularIndeterminate({color}) {
   return (
     <Box sx={{ display: "flex" }}>
-      <CircularProgress size={20} sx={{ color: "#222" }} />
+      <CircularProgress  size={20} sx={{ color: color }} />
     </Box>
   );
 }
