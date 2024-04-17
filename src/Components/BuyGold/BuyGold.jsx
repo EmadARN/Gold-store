35fcb6395e0810305840c55b4063e6ca76c1c242
Reza <rezafafaa78@gmail.com>
2024-04-17 12:19:06 +0330

    buy gold window location added

--- src/Components/BuyGold/BuyGold.jsx
+++ src/Components/BuyGold/BuyGold.jsx
@@ -161,7 +161,7 @@ const BuyGold = ({ walletDataToken, goldStockPrice }) => {
                     title: res.data.responseFA,
                     text: "در صورت بوجود آمدن مشکل با پشتیبانی تماس بگیرید ",
                     icon: "success",
-                  });
+                  }).then(() => window.location.reload());
                 })
                 .catch((err) => {
                   Swal.fire({
