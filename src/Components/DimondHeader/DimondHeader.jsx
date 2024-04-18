469a4ff38a3cc76b2ed529790e43527672ec9f99
emad <arabanemad@gmail.com>
2024-04-18 17:56:18 +0330

    Update DimondHeader

--- src/Components/DimondHeader/DimondHeader.jsx
+++ src/Components/DimondHeader/DimondHeader.jsx
@@ -1,58 +1,151 @@
-import { Box, Grid, styled } from "@mui/material";
+import { Box, Grid, styled, useMediaQuery } from "@mui/material";
 
 import React, { useEffect, useState } from "react";
 import ArrowBottom from "../ArrowBottom/ArrowBottom";
 import {
-    Boxin1,
-    Boxin2,
-    Boxin3,
-    Boxin4,
-    Boxin5,
-    BoxOneSx,
-    Li1,
-    Li2,
-    Li3,
-    Li4,
-    Li5,
-    Ul
+  Boxin1,
+  Boxin2,
+  Boxin3,
+  Boxin4,
+  Boxin5,
+  Boxin6,
+  Boxin7,
+  BoxOneSx,
+  Li1,
+  Li2,
+  Li3,
+  Li4,
+  Li5,
+  Ul,
 } from "@/Components/DimondHeader/Style";
-
 
 const DimondHeader = () => {
   const [fade, setFadeOut] = React.useState(false);
   useEffect(() => {
     setFadeOut(true);
   });
+  const isSmallScreen = useMediaQuery(
+    "(min-width: 200px) and (max-width: 375px)"
+  );
+  const isSmallScreenPlus = useMediaQuery(
+    "(min-width: 376px) and (max-width: 500px)"
+  );
+  const isMediumScreen = useMediaQuery(
+    "(min-width: 501px) and (max-width: 600px)"
+  );
+  const islargeScreen = useMediaQuery(
+    "(min-width: 601px) and (max-width:700px)"
+  );
+  const isXlargeScreen = useMediaQuery(
+    "(min-width: 701px) and (max-width:800px)"
+  );
+  const isXXlargeScreen = useMediaQuery(
+    "(min-width: 801px) and (max-width:1200px)"
+  );
+  const isXXXlargeScreen = useMediaQuery("(min-width: 1250px) ");
   return (
-    <Box
-      sx={BoxOneSx}
-    >
+    <Box sx={BoxOneSx}>
       <Ul
         sx={{
           opacity: fade ? 1 : 0,
           transition: fade ? "all 4s ease-in-out" : null,
         }}
       >
-        <Li1 className="li1">
+        <Box
+          sx={Li1(
+            isSmallScreen,
+            isSmallScreenPlus,
+            isMediumScreen,
+            islargeScreen,
+            isXlargeScreen,
+            isXXlargeScreen,
+            isXXXlargeScreen
+          )}
+          className="li1"
+        >
           <Boxin1></Boxin1>
-        </Li1>
+        </Box>
         <Grid sx={{}}>
           <ArrowBottom />
         </Grid>
-        <Li2 className="li2">
-          <Boxin2>بازار 24 ساعته</Boxin2>
-        </Li2>
-        <Li3 className="li3">
+        <Box
+          sx={Li2(
+            isSmallScreen,
+            isSmallScreenPlus,
+            isMediumScreen,
+            islargeScreen,
+            isXlargeScreen,
+            isXXlargeScreen,
+            isXXXlargeScreen
+          )}
+          className="li2"
+        >
+          {window.innerWidth < 600 ? (
+            <Boxin6></Boxin6>
+          ) : (
+            <Boxin2
+              sx={{
+                fontWeight: { xs: "none", sm: "bold" },
+                fontSize: { xs: "8px", sm: "15px", md: "16px", xl: "18px" },
+              }}
+            >
+              بازار 24 ساعته
+            </Boxin2>
+          )}
+        </Box>
+        <Box
+          sx={Li3(
+            isSmallScreen,
+            isSmallScreenPlus,
+            isMediumScreen,
+            islargeScreen,
+            isXlargeScreen,
+            isXXlargeScreen,
+            isXXXlargeScreen
+          )}
+          className="li3"
+        >
           <Boxin3></Boxin3>
-        </Li3>
-
-        <Li4 className="li4">
-          <Boxin4>امکان تحویل فیزیکی</Boxin4>
-        </Li4>
-
-        <Li5 className="li5">
+        </Box>
+        <Box
+          sx={Li4(
+            isSmallScreen,
+            isSmallScreenPlus,
+            isMediumScreen,
+            islargeScreen,
+            isXlargeScreen,
+            isXXlargeScreen,
+            isXXXlargeScreen
+          )}
+          className="li4"
+        >
+          {window.innerWidth < 600 ? (
+            <Boxin7></Boxin7>
+          ) : (
+            <Boxin4
+              sx={{
+                fontWeight: { xs: "none", sm: "bold" },
+                fontSize: { xs: "8px", sm: "15px", md: "16px", xl: "18px" },
+              }}
+            >
+              امکان تحویل فیزیکی
+            </Boxin4>
+          )}
+        </Box>
+        <Box
+          sx={Li5(
+            isSmallScreen,
+            isSmallScreenPlus,
+            isMediumScreen,
+            islargeScreen,
+            isXlargeScreen,
+            isXXlargeScreen,
+            isXXXlargeScreen
+          )}
+          className="li5"
+        >
           <Boxin5></Boxin5>
-        </Li5>
+        </Box>
       </Ul>
     </Box>
   );
--- src/Components/DimondHeader/Style.js
+++ src/Components/DimondHeader/Style.js
@@ -1,79 +1,415 @@
-import {styled} from "@mui/material/styles";
-import {img1, img2, img3} from "@/Components/DimondHeader/Data";
-import React, {useEffect} from "react";
+import { styled } from "@mui/material/styles";
+import { img1, img2, img3 } from "@/Components/DimondHeader/Data";
+import React, { useEffect } from "react";
 
 export const Ul = styled("ul")(({ theme }) => ({
   position: "relative",
   width: "600px",
   height: "150px",
 }));
-export const Li1 = styled("li")(({ theme }) => ({
-  listStyle: "none",
-  position: "absolute",
-  width: "160px",
-  height: "160px",
-  margin: "-85px",
-  transform: "rotate(45deg)",
-  top: 0,
-  left: 0,
-  transition: ".5s",
-  overfllow: "hidden",
-  backgroundColor: "#FFC436",
-}));
-export const Li2 = styled("li")(({ theme }) => ({
-  listStyle: "none",
-  position: "absolute",
-  width: "160px",
-  height: "160px",
-  transform: "rotate(45deg)",
-  transition: ".5s",
-  top: 0,
-  left: "0%",
-  margin: "50px",
-  overfllow: "hidden",
-  backgroundColor: "#1C1B19",
-  border: "1px solid  #FFC436",
-}));
-export const Li3 = styled("li")(({ theme }) => ({
-  listStyle: "none",
-  position: "absolute",
-  width: "160px",
-  height: "160px",
-  transform: "rotate(45deg)",
-  transition: ".5s",
-  top: 0,
-  left: "45%",
-  margin: "-85px",
-  overflow: "hidden",
-  backgroundColor: "#FFC436",
-}));
-export const Li4 = styled("li")(({ theme }) => ({
-  listStyle: "none",
-  position: "absolute",
-  width: "160px",
-  height: "160px",
-  transform: "rotate(45deg)",
-  transition: ".5s",
-  top: 0,
-  left: "45%",
-  margin: "50px",
-  overfllow: "hidden",
-  backgroundColor: "#1C1B19",
-  border: "1px solid  #FFC436",
-}));
-export const Li5 = styled("li")(({ theme }) => ({
-  listStyle: "none",
-  position: "absolute",
-  width: "160px",
-  height: "160px",
-  transform: "rotate(45deg)",
-  transition: ".5s",
-  top: 0,
-  left: "90%",
-  margin: "-85px",
-  overfllow: "hidden",
-  backgroundColor: "#FFC436",
-}));
+export const Li1 = (
+  isSmallScreen,
+  isSmallScreenPlus,
+  isMediumScreen,
+  islargeScreen,
+  isXlargeScreen,
+  isXXlargeScreen,
+  isXXXlargeScreen
+) => {
+  const style = {
+    listStyle: "none",
+    position: "absolute",
+    width: isSmallScreen
+      ? "0px"
+      : isSmallScreenPlus
+      ? "0px"
+      : isMediumScreen
+      ? "0"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    height: isSmallScreen
+      ? "0px"
+      : isSmallScreenPlus
+      ? "0px"
+      : isMediumScreen
+      ? "0x"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    margin: isSmallScreen
+      ? "-25px"
+      : isSmallScreenPlus
+      ? "-30px"
+      : isMediumScreen
+      ? "-35px"
+      : islargeScreen
+      ? "-55px"
+      : isXlargeScreen
+      ? "-65px"
+      : isXXlargeScreen
+      ? "-85px"
+      : isXXXlargeScreen
+      ? "-85px"
+      : "-85px",
+    transform: "rotate(45deg)",
+    top: 0,
+    left: isSmallScreen
+      ? "-12%"
+      : isSmallScreenPlus
+      ? "-6%"
+      : isMediumScreen
+      ? "-12%"
+      : islargeScreen
+      ? 0
+      : isXlargeScreen
+      ? 0
+      : isXXlargeScreen
+      ? 0
+      : isXXXlargeScreen
+      ? 0
+      : 0,
+    transition: ".5s",
+    overfllow: "hidden",
+    backgroundColor: "#FFC436",
+  };
+  return style;
+};
+export const Li2 = (
+  isSmallScreen,
+  isSmallScreenPlus,
+  isMediumScreen,
+  islargeScreen,
+  isXlargeScreen,
+  isXXlargeScreen,
+  isXXXlargeScreen
+) => {
+  const style = {
+    listStyle: "none",
+    position: "absolute",
+    width: isSmallScreen
+      ? "70px"
+      : isSmallScreenPlus
+      ? "70px"
+      : isMediumScreen
+      ? "100px"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    height: isSmallScreen
+      ? "70px"
+      : isSmallScreenPlus
+      ? "70px"
+      : isMediumScreen
+      ? "100px"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    transform: "rotate(45deg)",
+    transition: ".5s",
+    top: 0,
+    left: isSmallScreen
+      ? "-10%"
+      : isSmallScreenPlus
+      ? "-6%"
+      : isMediumScreen
+      ? "-12%"
+      : islargeScreen
+      ? 0
+      : isXlargeScreen
+      ? 0
+      : isXXlargeScreen
+      ? 0
+      : isXXXlargeScreen
+      ? 0
+      : 0,
+    margin: isSmallScreen
+      ? "35px"
+      : isSmallScreenPlus
+      ? "35px"
+      : isMediumScreen
+      ? "50px"
+      : islargeScreen
+      ? "50px"
+      : isXlargeScreen
+      ? "50px"
+      : isXXlargeScreen
+      ? "50px"
+      : isXXXlargeScreen
+      ? "50px"
+      : "50px",
+    overfllow: "hidden",
+    backgroundColor: "#1C1B19",
+    border: "1px solid  #FFC436",
+  };
+  return style;
+};
+export const Li3 = (
+  isSmallScreen,
+  isSmallScreenPlus,
+  isMediumScreen,
+  islargeScreen,
+  isXlargeScreen,
+  isXXlargeScreen,
+  isXXXlargeScreen
+) => {
+  const style = {
+    listStyle: "none",
+    position: "absolute",
+    width: isSmallScreen
+      ? "70px"
+      : isSmallScreenPlus
+      ? "70px"
+      : isMediumScreen
+      ? "100px"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    height: isSmallScreen
+      ? "70px"
+      : isSmallScreenPlus
+      ? "70px"
+      : isMediumScreen
+      ? "100px"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    transform: "rotate(45deg)",
+    transition: ".5s",
+    top: 0,
+    left: isSmallScreen
+      ? "35%"
+      : isSmallScreenPlus
+      ? "34%"
+      : isMediumScreen
+      ? "40%"
+      : islargeScreen
+      ? "38%"
+      : isXlargeScreen
+      ? "38%"
+      : isXXlargeScreen
+      ? "45%"
+      : isXXXlargeScreen
+      ? "45%"
+      : "45%",
+    margin: isSmallScreen
+      ? "-27px"
+      : isSmallScreenPlus
+      ? "-25px"
+      : isMediumScreen
+      ? "-39px"
+      : islargeScreen
+      ? "-55px"
+      : isXlargeScreen
+      ? "-63px"
+      : isXXlargeScreen
+      ? "-85px"
+      : isXXXlargeScreen
+      ? "-85px"
+      : "-85px",
+    overflow: "hidden",
+    backgroundColor: "#FFC436",
+  };
+  return style;
+};
+export const Li4 = (
+  isSmallScreen,
+  isSmallScreenPlus,
+  isMediumScreen,
+  islargeScreen,
+  isXlargeScreen,
+  isXXlargeScreen,
+  isXXXlargeScreen
+) => {
+  const style = {
+    listStyle: "none",
+    position: "absolute",
+    width: isSmallScreen
+      ? "70px"
+      : isSmallScreenPlus
+      ? "70px"
+      : isMediumScreen
+      ? "100px"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    height: isSmallScreen
+      ? "70px"
+      : isSmallScreenPlus
+      ? "70px"
+      : isMediumScreen
+      ? "100px"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    transform: "rotate(45deg)",
+    transition: ".5s",
+    top: 0,
+    left: isSmallScreen
+      ? "35%"
+      : isSmallScreenPlus
+      ? "37%"
+      : isMediumScreen
+      ? "40%"
+      : islargeScreen
+      ? "40%"
+      : isXlargeScreen
+      ? "40%"
+      : isXXlargeScreen
+      ? "47%"
+      : isXXXlargeScreen
+      ? "47%"
+      : "47%",
+    margin: isSmallScreen
+      ? "34px"
+      : isSmallScreenPlus
+      ? "37px"
+      : isMediumScreen
+      ? "50px"
+      : islargeScreen
+      ? "50px"
+      : isXlargeScreen
+      ? "45px"
+      : isXXlargeScreen
+      ? "45px"
+      : isXXXlargeScreen
+      ? "46px"
+      : "46px",
+    overfllow: "hidden",
+    backgroundColor: "#1C1B19",
+    border: "1px solid  #FFC436",
+  };
+  return style;
+};
+export const Li5 = (
+  isSmallScreen,
+  isSmallScreenPlus,
+  isMediumScreen,
+  islargeScreen,
+  isXlargeScreen,
+  isXXlargeScreen,
+  isXXXlargeScreen
+) => {
+  const style = {
+    listStyle: "none",
+    position: "absolute",
+    width: isSmallScreen
+      ? "0px"
+      : isSmallScreenPlus
+      ? "0px"
+      : isMediumScreen
+      ? "0"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    height: isSmallScreen
+      ? "0px"
+      : isSmallScreenPlus
+      ? "0px"
+      : isMediumScreen
+      ? "0"
+      : islargeScreen
+      ? "120px"
+      : isXlargeScreen
+      ? "140px"
+      : isXXlargeScreen
+      ? "160px"
+      : isXXXlargeScreen
+      ? "160px"
+      : "155px",
+    transform: "rotate(45deg)",
+    transition: ".5s",
+    top: 0,
+    left: isSmallScreen
+      ? "80%"
+      : isSmallScreenPlus
+      ? "78%"
+      : isMediumScreen
+      ? "78%"
+      : islargeScreen
+      ? "78%"
+      : isXlargeScreen
+      ? "78%"
+      : isXXlargeScreen
+      ? "91%"
+      : isXXXlargeScreen
+      ? "91%"
+      : "91%",
+    margin: isSmallScreen
+      ? "-20px"
+      : isSmallScreenPlus
+      ? "-28px"
+      : isMediumScreen
+      ? "-34px"
+      : islargeScreen
+      ? "-55px"
+      : isXlargeScreen
+      ? "-70px"
+      : isXXlargeScreen
+      ? "-85px"
+      : isXXXlargeScreen
+      ? "-85px"
+      : "-84px",
+    overfllow: "hidden",
+    backgroundColor: "#FFC436",
+  };
+  return style;
+};
+
 export const Boxin1 = styled("div")(({ theme }) => ({
   backgroundImage: `url(${img1})`,
   backgroundSize: "cover",
@@ -86,8 +422,25 @@ export const Boxin2 = styled("div")(({ theme }) => ({
   position: "absolute",
   top: "60px",
   right: "40px",
-  fontWeight: "bold",
-  fontSize: "18px",
+
+  color: "#fff",
+  animation: "span4 1.5s ease-in-out infinite alternate",
+  "@keyframes span4": {
+    "0%": {
+      opacity: "0",
+    },
+    "100%": {
+      opacity: "1",
+      scale: "1.2",
+    },
+  },
+  textShadow: "0px 2px 18px rgba(255, 196, 54, 1)",
+}));
+export const Boxin4 = styled("div")(({ theme }) => ({
+  transform: "rotate(-45deg)",
+  position: "absolute",
+  top: "60px",
+  right: "15px",
   color: "#fff",
   animation: "span4 1.5s ease-in-out infinite alternate",
   "@keyframes span4": {
@@ -108,26 +461,7 @@ export const Boxin3 = styled("div")(({ theme }) => ({
   height: "100%",
   transform: "rotate(360deg)",
 }));
-export const Boxin4 = styled("div")(({ theme }) => ({
-  transform: "rotate(-45deg)",
-  position: "absolute",
-  top: "60px",
-  right: "15px",
-  fontWeight: "bold",
-  fontSize: "18px",
-  color: "#fff",
-  animation: "span4 1.5s ease-in-out infinite alternate",
-  "@keyframes span4": {
-    "0%": {
-      opacity: "0",
-    },
-    "100%": {
-      opacity: "1",
-      scale: "1.2",
-    },
-  },
-  textShadow: "0px 2px 18px rgba(255, 196, 54, 1)",
-}));
+
 export const Boxin5 = styled("div")(({ theme }) => ({
   backgroundImage: `url(${img3})`,
   backgroundSize: "cover",
@@ -136,12 +470,25 @@ export const Boxin5 = styled("div")(({ theme }) => ({
   transform: "rotate(360deg)",
 }));
 
+export const Boxin6 = styled("div")(({ theme }) => ({
+  backgroundImage: `url(${img3})`,
+  backgroundSize: "cover",
+  width: "100%",
+  height: "100%",
+  transform: "rotate(360deg)",
+}));
+export const Boxin7 = styled("div")(({ theme }) => ({
+  backgroundImage: `url(${img1})`,
+  backgroundSize: "cover",
+  width: "100%",
+  height: "100%",
+  transform: "rotate(360deg)",
+}));
 export const BoxOneSx = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  "@media (max-width:912px)": {
-    display: "none",
-  },
-}
-
+  // "@media (max-width:912px)": {
+  //   display: "none",
+  // },
+};
