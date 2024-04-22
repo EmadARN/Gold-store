54d547a9cf8e1b12728b30ce0f828ed6fb28f204
emad <arabanemad@gmail.com>
2024-04-22 14:44:52 +0330

    Update StepperComputer &b StepperPhone

--- src/Components/StepperComputer/Style.js
+++ src/Components/StepperComputer/Style.js
@@ -1,36 +1,34 @@
-export const FirstBox ={
-    width: "100%",
-    display: "flex",
-    justifyContent: "space-evenly",
-    "@media (max-width:912px)": {
-      display: "none",
-    },
-}
+export const FirstBox = {
+  width: "100%",
+  display: "flex",
+  justifyContent: "space-evenly",
+  "@media (max-width:912px)": {
+    display: "none",
+  },
+};
 
+export const SteperStyle = {
+  display: "flex",
+  alignItems: "start",
+  justifyContent: "center",
+};
 
-export const SteperStyle ={
-    display: "flex",
-    alignItems: "start",
-    justifyContent: "center",
-}
+export const MapBox = {
+  display: "flex",
+  alignItems: "center",
+};
 
-export  const MapBox  = {
-    display: "flex",
-    alignItems: "center",
-}
-
-
-export const  GoldBox ={
-    boxShadow:
-    "#FFC436 0px 2px 30px,#FFC436 0px 7px 90px -3px,#FFC436 0px -3px 30px ",
-
+export const GoldBox = {
+  filter: "drop-shadow(0px 0px 15px #FFC436)",
   bgcolor: "rgb(39,37,35)",
   borderRadius: "8px",
   color: "#fff",
   width: "30%",
-}
-
+};
 
 export const TypographyStyle = {
-    mt: 5, mb: 1, p: 2, textAlign: "justify" 
-}
+  mt: 5,
+  mb: 1,
+  p: 2,
+  textAlign: "justify",
+};
