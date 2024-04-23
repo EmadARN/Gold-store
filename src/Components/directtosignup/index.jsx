a1dec0726350201a4036db9f62547cce942db6b6
RezaFarzipour <rezafafa78@gmail.com>
2024-04-23 04:48:47 -0700

    Direction component

---
+++ src/Components/directtosignup/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Typography ,Grid ,Box,Button} from '@mui/material'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Link from 'next/link'
+import { ButtonDir, MainDirGrid } from './style';
+const Direct_Sign_up = () => {
+  return (
+ <Grid sx={MainDirGrid}> <Link   href='/SignUp'><Button sx={ButtonDir} endIcon={<ArrowBackIosIcon sx={{mr:2}}/>}>برای شروع خرید و فروش طلا کلیک کنید</Button></Link></Grid>
+  )
+}
+
+export default Direct_Sign_up
---
+++ src/Components/directtosignup/style.js
@@ -0,0 +1,9 @@
+export const MainDirGrid ={
+    p:1.5,bgcolor:"#F1AB1F",borderRadius:"10px","&:hover":{bgcolor:"#d3961b"}
+}
+
+
+export const ButtonDir ={
+    color:"#111",fontSize:{xs:'15px',md:"20px"},fontFamily:'Yekan'
+    
+}
